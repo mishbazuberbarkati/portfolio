aa39a3189a625da5f89508d8a9865218c8789f1c
Mishba Zuber Barkati <barkati.mishba26@gmail.com>
2026-07-25 18:29:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const nextConfig = {
     output: "export",
   reactStrictMode: true,
+     trailingSlash: true,
   images: {
     unoptimized: true,
     formats: ["image/avif", "image/webp"],
       remotePatterns: [
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com', pathname: '/**' },
+      {     protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
     ],
   },
   basePath: "/portfolio",
